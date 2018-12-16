619644fa0aec9be878e17a4af07e120a8c247484
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-12-16 13:34:17 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      switch: true
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main-container">
+        <div className="keyboard" >
+          <div>Q</div>
+          <div>W</div>
+          <div>E</div>
+          <div>A</div>
+          <div>S</div>
+          <div>D</div>
+          <div>Z</div>
+          <div>X</div>
+          <div>C</div>
+        </div>
+        <div className="panel" >
+          <div className="panel-inside" >
+            <div className="switch" >
+              <h3>{this.state.switch ? "On" : "Off"}</h3>
+              <div onClick={() => this.setState({switch: !this.state.switch})} className={this.state.switch ? "switcher-on" : "switcher-off"} ></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
